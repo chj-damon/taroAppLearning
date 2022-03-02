5a52f6bb6cbd569153fb79444ee054f32c34da85
chj_damon <chjdamon@gmail.com>
2022-03-02 11:28:58 +0800

    feat: 增加性能优化相关配置

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ const config = {
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: [],
+  plugins: ['taro-plugin-compiler-optimization'],
   defineConstants: {},
   copy: {
     patterns: [],
